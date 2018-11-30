18d46804b2e3c337c165c3e259260ff985f72841
tashrafy <tashrafy91@gmail.com>
2018-11-29 20:37:30 -0500

    chore: removing client serve statements

--- server/express.js
+++ server/express.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const favicon = require('serve-favicon');
 const morgan = require('morgan');
 const compression = require('compression');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
-const path = require('path');
 const config = require('./environment');
 const passport = require('passport');
 const session = require('express-session');
@@ -14,9 +12,6 @@ const mongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
 
 module.exports = function(app) {
-  const env = app.get('env');
-
-  app.set('view engine', 'html');
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
@@ -29,18 +24,6 @@ module.exports = function(app) {
     saveUninitialized: true,
     store: new mongoStore({ mongooseConnection: mongoose.connection })
   }));
-
-  if (['production', 'staging'].includes(env)) {
-    app.use(favicon(path.join(config.root, process.cwd() + '/client/dist', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, process.cwd() + '/client/dist')));
-    app.set('appPath', config.root + process.cwd() + '/client/dist');
-  }
-
-  if (['development', 'test'].includes(env)) {
-    app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
-  }
 
   app.use(morgan('dev'));
   app.use(helmet());
