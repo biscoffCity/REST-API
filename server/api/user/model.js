b4d2e6f3fcfa00276f4a7bb0f6ef72b5a0b38c11
Lucius <tashrafy91@gmail.com>
2019-08-26 22:31:32 -0400

    chore(oauth): removing reddit connect apis

--- server/api/user/model.js
+++ server/api/user/model.js
@@ -24,7 +24,6 @@ const UserSchema = new Schema({
   provider: String,
   salt: String,
   facebook: {},
-  reddit: {},
   linkedin: {},
   google: {}
 });
--- server/auth/index.js
+++ server/auth/index.js
@@ -5,12 +5,10 @@ const User = require('../api/user/model');
 
 require('./facebook/passport').setup(User, config);
 require('./google/passport').setup(User, config);
-require('./reddit/passport').setup(User, config);
 require('./linkedin/passport').setup(User, config);
 
 router.use('/facebook', require('./facebook'));
 router.use('/google', require('./google'));
-router.use('/reddit', require('./reddit'));
 router.use('/linkedin', require('./linkedin'));
 
 module.exports = router;
